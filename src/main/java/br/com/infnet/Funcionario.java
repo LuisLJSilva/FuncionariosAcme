62457beb0c841354ad8b35388a077c7805934e8c
Luís Roberto <luisldjsilva@gmail.com>
2024-06-15 10:17:08 -0300

    Commit segunda entrega
    Este commit visa Identificar os Bad Smells e aplicar os princípios de Clean Code enquanto refatorar o código existente.

--- src/main/java/br/com/infnet/Funcionario.java
+++ src/main/java/br/com/infnet/Funcionario.java
@@ -23,48 +23,24 @@ public class Funcionario {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public List<String> getTelefones() {
         return telefones;
-    }
-
-    public void setTelefones(List<String> telefones) {
-        this.telefones = telefones;
     }
 
     public String getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
     public double getSalario() {
         return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
     }
 
     public Setor getSetor() {
         return setor;
     }
 
-    public void setSetor(Setor setor) {
-        this.setor = setor;
-    }
-
     public Cargo getCargo() {
         return cargo;
-    }
-
-    public void setCargo(Cargo cargo) {
-        this.cargo = cargo;
     }
 
     public void reajustarSalario(double percentual) {
